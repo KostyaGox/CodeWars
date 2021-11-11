6b9744486619512ab03d2f77efac8fff7563c62b
Kostya <kolya7387@gmail.com>
2021-11-11 23:57:42 +0300

    initial commit

---
+++ 26,.js
@@ -0,0 +1,14 @@
+function evaporator(content, evap_per_day, threshold){
+    let days = 0;
+    let currentLvl = 100;
+    let lowestLvl = 100 * (threshold * .01);
+    while (currentLvl >= lowestLvl){
+      days++;
+      currentLvl -= (currentLvl * (evap_per_day * .01));
+      // console.log(currentLvl);
+    }
+  
+    return days;
+  }
+  
+  console.log(evaporator(10,10,10) === 22);
