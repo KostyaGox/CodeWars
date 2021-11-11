4211a4e739a9e7729cd76182a7dda27f9bf38fc7
Kostya <kolya7387@gmail.com>
2021-11-11 23:38:14 +0300

    initial commit

---
+++ 22.js
@@ -0,0 +1,15 @@
+function bandNameGenerator(str) {
+    if (str[0] === str[str.length - 1]){
+      return double(str);
+    } else {
+      return addThe(str);
+    }
+  }
+  
+  let addThe = (noun) => {
+    return "The " + noun.slice(0,1).toUpperCase() + noun.slice(1);
+  }
+  
+  let double = (noun) => {
+    return noun.slice(0,1).toUpperCase() + noun.slice(1) + noun.slice(1);
+  }
